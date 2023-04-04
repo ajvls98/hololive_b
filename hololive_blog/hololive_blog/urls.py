4ee3c4c1fadb27236cb1915ecea2727e6c8d5b41
kano <ajvls98@gmail.com>
2023-04-04 16:52:30 +0900

    secret_key error

--- hololive_blog/hololive_blog/urls.py
+++ hololive_blog/hololive_blog/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('accountapp.urls')),
     path('account/', include('accountapp.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
