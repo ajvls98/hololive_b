ffbe58940a2acaa04746d009f41ed0df36d9efa7
kano <ajvls98@gmail.com>
2023-04-05 10:55:09 +0900

    🗃️ django makemigration, migrate (database)

--- hololive_blog/hololive_blog/settings.py
+++ hololive_blog/hololive_blog/settings.py
@@ -106,7 +106,12 @@ WSGI_APPLICATION = 'hololive_blog.wsgi.application'
 #         default='sqlite:////tmp/my-tmp-sqlite.db'
 #     )
 # }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # # Password validation
 # # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
